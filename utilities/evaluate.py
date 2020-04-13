42cce61ab441b4ede3a07baf07f96094f084b332
Luca <rispoli997@gmail.com>
2020-04-13 19:30:16 +0200

    added evaluateText for mean and rhymes scores

---
+++ utilities/evaluate.py
@@ -0,0 +1,33 @@
+import numpy as np
+import unidecode
+
+def evaluateText(filepath='../resources/cantica.txt'):
+    score = dict()
+    result = list()
+    comparisons = 0
+    with open(filepath,encoding="utf-8") as dc:
+        lines = list(line for line in (l.strip() for l in dc.readlines()) if line)  # Remove lines with only newline
+        words_per_line = [len(line) for line in lines]
+        score['mean'] = np.mean(words_per_line)
+        score['variance'] = np.var(words_per_line)
+        sentences = [line for line in lines if 'Canto' not in line]
+        # Compare lines, the rhymes must be of the form: ABA BCB 
+        for i in range(0,len(sentences)-3,3):
+            result.append(check_rime(sentences[i],sentences[i+2])) # Compare A - A
+            result.append(check_rime(sentences[i+1],sentences[i+3])) # Compare B - B
+            comparisons += 2
+        score['rhymes'] = comparisons/sum(result) # Normalize result
+        #print(score)
+        
+def check_rime(first,second):
+    return last_syll(first) == last_syll(second)
+    
+
+def last_syll(word):
+    vowels = ['a','e','i','o','u']
+    word = unidecode.unidecode(word)
+    for i in reversed(range(len(word)-1)):
+        if word[i] in vowels:
+            return word[i+1:]
+
+
